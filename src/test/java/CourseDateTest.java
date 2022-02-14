13e62469bed8e689a4f92a18c0a1a5b8449ebe63
Helmut87 <vplistik@gmail.com>
2022-02-14 19:52:14 +0300

    Add tests

--- src/test/java/CourseDateTest.java
+++ src/test/java/CourseDateTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.text.ParseException;
@@ -7,10 +8,11 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.*;
 
-public class CourseDateTest extends BaseSetUp {
+public class CourseDateTest {
 
     private static final String URL = "https://otus.ru";
-//    @Test
+
+        @Test
     public String findCourse(String oldDateString) throws ParseException {
 
         LocalDate current_date = LocalDate.now();
@@ -22,10 +24,10 @@ public class CourseDateTest extends BaseSetUp {
     }
 
     public static void main(String[] args) throws ParseException {
-
+        WebDriver driver = WebDriverFactory.getWebDriver("chrome");
         driver.get(URL);
         Date nowDate = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM", Locale.getDefault());
         ConvertStringToDate convertStringToDate = new ConvertStringToDate();
 
         List<WebElement> listOfCourses = driver.findElements(By.xpath(".//div[@class = 'lessons__new-item-container']"));
@@ -52,55 +54,28 @@ public class CourseDateTest extends BaseSetUp {
 
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
-//            String tab = convertStringToDate.convertDate(entry.getValue());
-            Date date = formatter.parse(convertStringToDate.convertDate(entry.getValue()));
-//            map.put(key, tab);
+            Date date = formatter.parse(entry.getValue());
             map1.put(key, date.getTime());
         }
-        System.out.println(map1);
+
+        Long max = map1.values()
+                .stream()
+                .reduce(Long::max).orElse((long) -1);
+
+        for (Map.Entry<String, Long> entry : map1.entrySet()) {
+            if (entry.getValue().toString().contains(String.valueOf(max))) {
+                System.out.println("Самый поздний курс: " + entry.getKey());
+            }
+        }
+
+        Long min = map1.values()
+                .stream()
+                .reduce(Long::min).orElse((long) -1);
+
+        for (Map.Entry<String, Long> entry : map1.entrySet()) {
+            if (entry.getValue().toString().contains(String.valueOf(min))) {
+                System.out.println("Самый ранний курс: " + entry.getKey());
+            }
+        }
     }
 }
-
-
-//        allDates.removeIf(s -> s.equals("О дате старта будет объявлено позже"));
-//        allDates.removeIf(x -> x.startsWith("В"));
-//
-//        Stream<String> stringStream = stringList.stream();
-//        String coolest = stringStream.reduce((a,b)->
-//                coolnessIndex(a) > coolnessIndex(b) ? a:b;).get()
-//
-//
-
-//    List<WebElement> listOfCourses = driver.findElements(By.xpath(".//div[@class = 'lessons__new-item-container']"));
-//
-//    Map<String, String> map = new HashMap<>();
-//    Map<String, Date> map1 = new HashMap<>();
-//
-//        for (WebElement listOfCourse : listOfCourses) {
-//                String courseTitle = listOfCourse.findElement(By.className("lessons__new-item-title")).getText();
-//                String courseDate = listOfCourse.findElement(By.className("lessons__new-item-time")).getText();
-//                map.put(courseTitle, courseDate);
-//                }
-//
-//                map.entrySet()
-//                .removeIf(entry -> entry.getValue().matches("^\\d{1,3}\\s\\\\?месяцев"));
-//                map.entrySet()
-//                .removeIf(entry -> entry.getValue().matches("^\\d{1,3}\\s\\\\?месяц"));
-//                map.entrySet()
-//                .removeIf(entry -> entry.getValue().matches("^\\d{1,3}\\s\\\\?месяца"));
-//                map.entrySet()
-//                .removeIf(entry -> entry.getValue().startsWith("В"));
-//                map.entrySet()
-//                .removeIf(entry -> entry.getValue().startsWith("О"));
-//
-//                for (Map.Entry<String, String> entry : map.entrySet()) {
-//        String key = entry.getKey();
-//        Date date = oldDateFormat.parse(entry.getValue());
-//        map1.put(key, date);
-//        }
-//
-//        List<Date> od = map1.values().stream()
-//        .filter(d -> d.after(typedDate))
-//        .collect(Collectors.toList());
-
-//TO DO Use stream.reduce to find max/min course start date
